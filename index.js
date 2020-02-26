3e146758344f20c8d4bfadca3e5e546a82bec707
cesar galindo <csgalindos@hotmail.com>
2020-02-25 23:39:07 -0500

    [updated] first view in the web

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 'use strict'
 
 const { graphql, buildSchema } = require('graphql')
+const express = require ('express')
+const gqlMiddleware = require('express-graphql')
+
+const app = express()
+const port = process.env.port || 3000
+
 
 // definiendo el esquema
 const schema = buildSchema(`
@@ -20,7 +26,12 @@ const resolvers = {
   }
 }
 
-// Ejecutar el query hello
-graphql(schema, '{ saludo }', resolvers).then((data) => {
-  console.log(data)
+app.use('/api', gqlMiddleware({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
+}))
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`);
 })
