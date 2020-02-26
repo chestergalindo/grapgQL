5f85fb5910969fa66bf6fb4d5ae6e49cac8a0a94
cesar galindo <csgalindos@hotmail.com>
2020-02-25 23:57:34 -0500

    [updated] make a files of the shema and resolvers

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
 'use strict'
 
-const { graphql, buildSchema } = require('graphql')
-const express = require ('express')
+const { buildSchema } = require('graphql')
+const express = require('express')
 const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/ resolvers')
 
 const app = express()
 const port = process.env.port || 3000
 
-
 // definiendo el esquema
-const schema = buildSchema(`
-  type Query {
-    hello: String
-    saludo: String
-  }
-`)
-
-// Configurar los resolvers
-const resolvers = {
-  hello: () => {
-    return 'Hola Mundo'
-  },
-  saludo: () => {
-    return 'Hola a todos'
-  }
-}
+const schema = buildSchema(
+  readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8'
+  )
+)
 
 app.use('/api', gqlMiddleware({
   schema: schema,
@@ -33,5 +25,5 @@ app.use('/api', gqlMiddleware({
 }))
 
 app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}/api`);
+  console.log(`Server is listening at http://localhost:${port}/api`)
 })
